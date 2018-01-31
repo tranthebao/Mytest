6f58545c958a8478a8fbaa672a439d87a20a43d6
tranthebao <tranthebaocntt31b@gmail.com>
2018-01-31 11:23:18 +0700

    upload file buffers

---
+++ buffers.js
@@ -0,0 +1,13 @@
+var buf = new Buffer(256);
+var buf1 =  new Buffer([10,20,30,40]);
+var buf2 = new Buffer("simply easy learning", "utf-8");
+var len = buf.write("simple easy learning");
+console.log("Octets writen :"+ len);
+var buf3 = new Buffer(26);
+for(var i = 0 ; i<26 ;i++){
+    buf3[i]=i+97;
+}
+//console.log(buf3.toString('ascii'));
+console.log(buf3.toString('ascii',0,5));
+//console.log(buf3.toString('utf-8',0,5));
+//console.log(buf3.toString(undefined,0,5));
