0fc647653390ee2cc8630776a61f229f1e19f965
tranthebao <tranthebaocntt31b@gmail.com>
2018-01-31 09:01:23 +0700

    update file and modify demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,22 @@
-var express = require('express');
+/*var express = require('express');
+var fs=require('fs');
+var data = fs.readFileSync('input.txt');
+fs.readFile('input.txt',function(err, dataF){
+    if(err)return console.log(err);
+    console.log(dataF.toString());
+});
+console.log(data.toString());
+console.log("Program Ended");*/
+var event = require('events');
+var eventEmiter = new event.EventEmitter();
+var connectHandler = function connecttted(){
+    console.log('connection successful');
+    eventEmiter.emit('data_received');
+}
+eventEmiter.on('connection', connectHandler);
+eventEmiter.on('data_received',function(){
+    console.log('data_received successful');
+});
+//dot it
+eventEmiter.emit('connection');
+console.log("Program Ended");
