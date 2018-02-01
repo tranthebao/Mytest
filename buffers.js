fa3031ed90b0ba1b645dba169134834f425e6539
tranthebao <tranthebaocntt31b@gmail.com>
2018-02-01 09:56:32 +0700

    remove comment

--- buffers.js
+++ buffers.js
@@ -7,7 +7,7 @@ var buf3 = new Buffer(26);
 for(var i = 0 ; i<26 ;i++){
     buf3[i]=i+97;
 }
-//console.log(buf3.toString('ascii'));
+console.log(buf3.toString('ascii'));
 console.log(buf3.toString('ascii',0,5));
-//console.log(buf3.toString('utf-8',0,5));
-//console.log(buf3.toString(undefined,0,5));
+console.log(buf3.toString('utf-8',0,5));
+console.log(buf3.toString(undefined,0,5));
